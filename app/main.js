d51aeeab11664ca9cf7dbc5516cb02dc791b16cd
Steve Kinney <hello@stevekinney.net>
2016-11-27 15:25:20 -0700

    Prompt user to select a file on application start

--- app/main.js
+++ app/main.js
@@ -9,9 +9,20 @@ app.on('ready', () => {
 
   mainWindow.once('ready-to-show', () => {
     mainWindow.show();
+    getFileFromUserSelection();
   });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 });
+
+const getFileFromUserSelection = () => {
+  const files = dialog.showOpenDialog({
+    properties: ['openFile']
+  });
+
+  if (!files) { return; }
+
+  console.log(files);
+};
