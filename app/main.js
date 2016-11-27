17c0d7df2ee1394fafaee755d9df3b807c65e663
Steve Kinney <hello@stevekinney.net>
2016-11-27 15:03:00 -0700

    Load the window gracefully

--- app/main.js
+++ app/main.js
@@ -3,9 +3,13 @@ const { app, BrowserWindow } = require('electron');
 let mainWindow = null;
 
 app.on('ready', () => {
-  mainWindow = new BrowserWindow();
+  mainWindow = new BrowserWindow({ show: false });
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
 
   mainWindow.on('closed', () => {
     mainWindow = null;
