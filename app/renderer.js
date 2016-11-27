647e81ceaeb6b250226081c7ed818e82378b0e35
Steve Kinney <hello@stevekinney.net>
2016-11-27 15:27:26 -0700

    Cache selectors for each button

--- app/renderer.js
+++ app/renderer.js
@@ -2,6 +2,13 @@ const marked = require('marked');
 
 const markdownView = document.querySelector('#markdown');
 const htmlView = document.querySelector('#html');
+const newFileButton = document.querySelector('#new-file');
+const openFileButton = document.querySelector('#open-file');
+const saveMarkdownButton = document.querySelector('#save-markdown');
+const revertButton = document.querySelector('#revert');
+const saveHtmlButton = document.querySelector('#save-html');
+const showFileButton = document.querySelector('#show-file');
+const openInDefaultButton = document.querySelector('#open-in-default');
 
 const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
