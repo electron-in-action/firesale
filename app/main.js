a2850d0f097e114fd72346771312cf205808129f
Steve Kinney <hello@stevekinney.net>
2017-01-02 13:36:38 -0700

    Add the basic application menu

--- app/main.js
+++ app/main.js
@@ -1,10 +1,12 @@
-const { app, BrowserWindow, dialog } = require('electron');
+const { app, BrowserWindow, dialog, Menu } = require('electron');
+const applicationMenu = require('./application-menu');
 const fs = require('fs');
 
 const windows = new Set();
 const openFiles = new Map();
 
 app.on('ready', () => {
+  Menu.setApplicationMenu(applicationMenu);
   createWindow();
 });
 
