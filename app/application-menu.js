0f8ae6660bc532da04f75e0bd004acfe7f99a106
Steve Kinney <hello@stevekinney.net>
2017-01-02 13:36:38 -0700

    Add the basic application menu

--- app/application-menu.js
+++ app/application-menu.js
@@ -1,10 +1,72 @@
-const { app, BrowserWindow, Menu, shell } = require('electron');
+const { app, dialog, Menu, shell } = require('electron');
 const mainProcess = require('./main');
 
-const template = [ //B
+const template = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Open File',
+        accelerator: 'CommandOrControl+O',
+        click(item, focusedWindow) {
+          if (focusedWindow) {
+            return mainProcess.getFileFromUser(focusedWindow);
+          }
+
+          const newWindow = mainProcess.createWindow();
+
+          newWindow.on('show', () => {
+            mainProcess.getFileFromUser(newWindow);
+          });
+        },
+      },
+      {
+        label: 'Save File',
+        accelerator: 'CommandOrControl+S',
+        click(item, focusedWindow) {
+          if (!focusedWindow) {
+            return dialog.showErrorBox(
+              'Cannot Save or Export',
+              'There is currently no active document to save or export.'
+            );
+          }
+          mainProcess.saveMarkdown(focusedWindow);
+        },
+      },
+      {
+        label: 'Export HTML',
+        accelerator: 'Shift+CommandOrControl+S',
+        click(item, focusedWindow) {
+          if (!focusedWindow) {
+            return dialog.showErrorBox(
+              'Cannot Save or Export',
+              'There is currently no active document to save or export.'
+            );
+          }
+          mainProcess.saveHtml(focusedWindow);
+        },
+      },
+    ],
+  },
   {
     label: 'Edit',
     submenu: [
+      {
+        label: 'Undo',
+        accelerator: 'CommandOrControl+Z',
+        role: 'undo',
+      },
+      {
+        label: 'Redo',
+        accelerator: 'Shift+CommandOrControl+Z',
+        role: 'redo',
+      },
+      { type: 'separator' },
+      {
+        label: 'Cut',
+        accelerator: 'CommandOrControl+X',
+        role: 'cut',
+      },
       {
         label: 'Copy',
         accelerator: 'CommandOrControl+C',
@@ -15,6 +77,93 @@ const template = [ //B
         accelerator: 'CommandOrControl+V',
         role: 'paste',
       },
-    ]
+      {
+        label: 'Select All',
+        accelerator: 'CommandOrControl+A',
+        role: 'selectall',
+      },
+    ],
+  },
+  {
+    label: 'Window',
+    submenu: [
+      {
+        label: 'Minimize',
+        accelerator: 'CommandOrControl+M',
+        role: 'minimize',
+      },
+      {
+        label: 'Close',
+        accelerator: 'CommandOrControl+W',
+        role: 'close',
+      },
+    ],
+  },
+    {
+    label: 'Help',
+    role: 'help',
+    submenu: [
+      {
+        label: 'Visit Website',
+        click() { /* To be implemented */ }
+      },
+      {
+        label: 'Toggle Developer Tools',
+        click(item, focusedWindow) {
+          if (focusedWindow) focusedWindow.webContents.toggleDevTools();
+        }
+      }
+    ],
   }
 ];
+
+if (process.platform === 'darwin') {
+  const name = 'Firesale';
+  template.unshift({
+    label: name,
+    submenu: [
+      {
+        label: `About ${name}`,
+        role: 'about',
+      },
+      { type: 'separator' },
+      {
+        label: 'Services',
+        role: 'services',
+        submenu: [],
+      },
+      { type: 'separator' },
+      {
+        label: `Hide ${name}`,
+        accelerator: 'Command+H',
+        role: 'hide',
+      },
+      {
+        label: 'Hide Others',
+        accelerator: 'Command+Alt+H',
+        role: 'hideothers',
+      },
+      {
+        label: 'Show All',
+        role: 'unhide',
+      },
+      { type: 'separator' },
+      {
+        label: `Quit ${name}`,
+        accelerator: 'Command+Q',
+        click() { app.quit(); }, // A
+      },
+    ],
+  });
+
+  const windowMenu = template.find(item => item.label === 'Window'); // B
+  windowMenu.submenu.push(
+    { type: 'separator' },
+    {
+      label: 'Bring All to Front',
+      role: 'front',
+    }
+  );
+}
+
+module.exports = Menu.buildFromTemplate(template);
