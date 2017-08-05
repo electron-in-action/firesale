941e41a042cf5f16dfcb922c946d1af7681e4364
Steve Kinney <hello@stevekinney.net>
2017-08-05 14:29:47 -0600

    Apply changes from Chapter 6

--- app/renderer.js
+++ app/renderer.js
@@ -19,6 +19,8 @@ const openInDefaultButton = document.querySelector('#open-in-default');
 let filePath = null;
 let originalContent = '';
 
+const isDifferentContent = (content) => content !== markdownView.value;
+
 const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
@@ -52,7 +54,7 @@ const updateUserInterface = (isEdited) => {
 markdownView.addEventListener('keyup', (event) => {
   const currentContent = event.target.value;
   renderMarkdownToHtml(currentContent);
-  updateUserInterface(currentContent !== originalContent)
+  updateUserInterface(currentContent !== originalContent);
 });
 
 newFileButton.addEventListener('click', () => {
@@ -92,7 +94,7 @@ ipcRenderer.on('show-file', showFile);
 ipcRenderer.on('open-in-default', openInDefaultApplication);
 
 ipcRenderer.on('file-opened', (event, file, content) => {
-  if (currentWindow.isDocumentEdited()) {
+  if (currentWindow.isDocumentEdited() && isDifferentContent(content)) {
     const result = remote.dialog.showMessageBox(currentWindow, {
       type: 'warning',
       title: 'Overwrite Current Unsaved Changes?',
@@ -112,6 +114,7 @@ ipcRenderer.on('file-opened', (event, file, content) => {
 });
 
 ipcRenderer.on('file-changed', (event, file, content) => {
+  if (isDifferentContent(content)) return;
   const result = remote.dialog.showMessageBox(currentWindow, {
     type: 'warning',
     title: 'Overwrite Current Unsaved Changes?',
